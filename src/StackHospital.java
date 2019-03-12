9812a525f6dfd4ced694900a7e270e6c2b312f17
Skyler Sprecker <saspreck@gmail.com>
2019-03-11 21:44:56 -0500

    Finish StackHospital class

---
+++ src/StackHospital.java
@@ -0,0 +1,71 @@
+import java.util.Stack;
+
+/**
+ * Child class of Hospital.java that uses a stack to keep track of patients. 
+ * A stack organizes data by LIFO, last in, first out.
+ * @author skylersprecker
+ * @version 2019-03-11
+ * @param <PatientType>
+ */
+public class StackHospital<PatientType> extends Hospital<PatientType> {
+	
+	// Declares a stack to be used in the object
+	private Stack<PatientType> patients;
+	
+	/**
+	 * Constructs a StackHospital object and initializes a stack
+	 */
+	public StackHospital() {
+		patients = new Stack<PatientType>();
+	}
+	
+	/**
+	 * Implements addPatient method defined in the Hospital class
+	 * 
+	 * @param patient	The patient to be added
+	 */
+	public void addPatient(PatientType patient) {
+		patients.push(patient);
+	}
+	
+	/**
+	 * Implements nextPatient method defined in Hospital class
+	 * 
+	 * @return The patient to be treated next
+	 */
+	public PatientType nextPatient() {
+		return patients.get(0);
+	}
+	
+	/**
+	 * Implements treatNextPatient method defined in Hospital class
+	 * 
+	 * @return The patient receiving treatment
+	 */
+	public PatientType treatNextPatient() {
+		return patients.pop();
+	}
+	
+	/**
+	 * Implements numPatients method defined in Hospital class
+	 * 
+	 * @return The number of patients yet to be treated
+	 */
+	public int numPatients() {
+		return patients.size();
+	}
+	
+	/**
+	 * Implements hospitalType method defined in Hospital class
+	 * 
+	 * @return The name of the class/type of hospital
+	 */
+	public String hospitalType() {
+		return "StackHospital";
+	}
+	
+	public String allPatientInfo() {
+		return patients.toString();
+	}
+
+}
---
+++ src/Todo.java
@@ -0,0 +1,35 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-03-11
+OU ID_______________: 113384038
+OU EMAIL____________: saspreck@ou.edu
+
+Tasks:
+1. complete StackHospital constructor
+2. complete StackHospital addPatient method
+3. complete StackHospital nextPatient method
+4. complete StackHospital treatNextPatient method 
+5. complete StackHospital numPatients method
+6. complete StackHospital hospitalType method
+7. complete StackHospital allPatientInfo method
+
+Estimated Time (minutes):
+1. 5
+2. 10
+3. 6
+4. 3
+5. 5
+6. 3
+7. 7
+
+Actual Time (minutes):
+1. 5
+2. 6
+3. 4
+4. 3
+5. 4
+6. 5
+7. 3
+
+
+*/}
